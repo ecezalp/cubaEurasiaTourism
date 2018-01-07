ff2f53083844d7431c6afab7abd6803436fe3497
Ece Ozalp <ozale272@newschool.edu>
2018-01-06 22:52:16 -0500

    finalizes navbar design

--- src/main/javascript/components/navbar.jsx
+++ src/main/javascript/components/navbar.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import englishFlag from '../../resources/static/images/english_flag.png';
+import turkishFlag from '../../resources/static/images/turkish_flag.png';
+
 
 export default function NavBar({isEnglish}) {
 
@@ -9,27 +12,22 @@ export default function NavBar({isEnglish}) {
 
   const getLogo = () => {
     return <div className="navbar-logo">
-      <span><span className="navbar-logo-bold">Eurasia</span> Tourism </span>
-      <div className="navbar-date">Since 1990</div>
+      <span><span className="navbar-logo-bold">EURASIA</span> TOURISM </span>
+      <div className="navbar-date">since 1991</div>
     </div>
   };
 
-  const getLanguages = () => {
-    return <ul className="navbar-languages">
-      <li><Link to="/en">English</Link></li>
-      <li><Link to="/tr">Türkçe</Link></li>
-    </ul>
-  };
-
-  const getAboutUs = () => {
+  const getNavbarTop = () => {
     let aboutUsHelper = Object.assign({},
       {true: {0: "About Us", 1: "Contact Us"}},
       {false: {0: "Hakkımızda", 1: "Iletişim"}});
 
-    return <ul className="navbar-about-us">
-      <li><Link to={`${linkPrefix}/about-us`}>{aboutUsHelper[isEnglish][0]}</Link></li>
-      <li><Link to={`${linkPrefix}/contact-us`}>{aboutUsHelper[isEnglish][1]}</Link></li>
-    </ul>
+    return <span className="navbar-about-us"><ul>
+      <li><Link to={`${linkPrefix}/about-us`} className="navbar-us">{aboutUsHelper[isEnglish][0]}</Link></li>
+      <li><Link to={`${linkPrefix}/contact-us`} className="navbar-us">{aboutUsHelper[isEnglish][1]}</Link></li>
+       <li><Link to="/tr"><img className="navbar-flag" src={turkishFlag}/></Link></li>
+      <li><Link to="/en"><img className="navbar-flag" src={englishFlag}/></Link></li>
+    </ul></span>
   };
 
   const getMenuItems = () => {
@@ -37,11 +35,11 @@ export default function NavBar({isEnglish}) {
       {true: {0: "Places to Go", 1: "Things to Do", 2: "Travel Blog"}},
       {false: {0: "Gideceklerimiz", 1: "Yapacaklarımız", 2: "Yolculuklarımız"}});
 
-    return <ul className="navbar-menu-items">
+    return <span className="navbar-menu-items"><ul>
       <li><Link to={`${linkPrefix}/places-to-go`}>{menuItemsHelper[isEnglish][0]}</Link></li>
       <li><Link to={`${linkPrefix}/things-to-do`}>{menuItemsHelper[isEnglish][1]}</Link></li>
       <li><Link to={`${linkPrefix}/travel-blog`}>{menuItemsHelper[isEnglish][2]}</Link></li>
-    </ul>
+    </ul></span>
   };
 
   return (
@@ -50,8 +48,9 @@ export default function NavBar({isEnglish}) {
         {getLogo()}
       </div>
       <div className="navbar-right">
-        {getAboutUs()}
-        {getLanguages()}
+        <div className="navbar-top">
+          {getNavbarTop()}
+        </div>
         {getMenuItems()}
       </div>
     </div>
