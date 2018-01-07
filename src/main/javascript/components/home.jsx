7f0a1d75fd0f126934da3e9e2d1b4b6b9febe046
Ece Ozalp <ozale272@newschool.edu>
2018-01-06 22:52:19 -0500

    add quote for Namibia

--- src/main/javascript/components/home.jsx
+++ src/main/javascript/components/home.jsx
@@ -9,10 +9,17 @@ export default function Home ({isEnglish}) {
 
         <div className="mainPageLeadPhoto">
           <div className="leadPhotoCaption"></div>
-          <div className="captionSection">Namibia</div>
+          <div className="captionSection">{isEnglish ? "Namibia" : "Namibya"}</div>
           <div className="layerBGColor"></div>
         </div>
 
-
+        <div className="shortDescCover">
+          <div className="quoteCover">
+            “I never knew of a morning in Africa<br/>when I woke up that I was not happy.”
+          </div>
+          <div className="quoteAuthor">
+            <i>Ture at First Light</i> by Hemingway
+          </div>
+        </div>
       </div>);
 }
