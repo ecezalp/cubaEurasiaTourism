f681757a5baa8d2ca8dd9587cab99187649594f5
Won Hong <whong@outlook.com>
2018-01-06 22:37:27 -0500

    updates namibia map and descriptions

--- src/main/javascript/components/home.jsx
+++ src/main/javascript/components/home.jsx
@@ -19,6 +19,50 @@ export default class Home extends React.Component {
     </div>
   };
 
+  getNamibiaDesc() {
+    let isEnglish = this.props.isEnglish;
+    let namibiaDesc = {
+      true: {
+        0: "We travel looking for ourselves.",
+        1: "We long for where we came from.",
+        2: "Namibia is not just a place but it's a sensation",
+        3: "The touch of wilderness rekindles",
+        4: "our sense of belonging to nature.",
+        5: "The Namib desert,",
+        6: "the rich ecosystem with unusual plants,",
+        7: "monoliths, caves, sinkholes, waterfalls,",
+        8: "lakes morph into a unique sense of Namibian identity.",
+        9: "Think about it. Things will not be same as before.",
+      },
+      false: {
+        0: "Kendimize bakmak için seyahat ediyoruz.",
+        1: "Nereden geldiğimizi uzun süre bekliyoruz.",
+        2: "Namibya sadece bir yer değil, aynı zamanda bir sansasyon",
+        3: "Vahşi doğanın dokunuşları yeniden canlanır",
+        4: "doğaya aid olma duygumuz.",
+        5: "Namib çölünün enginliği,",
+        6: "olağandışı bitkilerle zengin ekosistem,",
+        7: "monolitler, mağaralar, düdensler, şelaleler,",
+        8: "göller, Namibya kimliğinin eşsiz bir anlamda morph.",
+        9: "Bunu düşün. Her şey eskisi gibi olmayacak.",
+      },
+    };
+
+    return <div className="namibia-description-container">
+      <div className="namibia-description">
+        {namibiaDesc[isEnglish][0]}<br/>
+        {namibiaDesc[isEnglish][1]}<br/>
+        <b>{namibiaDesc[isEnglish][2]}</b><br/>
+        {namibiaDesc[isEnglish][3]}<br/>
+        {namibiaDesc[isEnglish][4]}<br/>
+        <b>{namibiaDesc[isEnglish][9]}</b><br/>
+      </div>
+      <div className="map-container">
+        {africaMap()}
+      </div>
+    </div>
+  }
+
   getQuote() {
     let isEnglish = this.props.isEnglish;
     let quoteHelper = {
@@ -26,7 +70,7 @@ export default class Home extends React.Component {
         0: "I never knew of a morning in Africa",
         1: "when I woke up that I was not happy.",
         2: "True at First Light",
-        3: "Ernest Hemingway"
+        3: "by Ernest Hemingway"
       },
       false: {
         0: "Bir sabah bilmedim ki",
@@ -36,20 +80,15 @@ export default class Home extends React.Component {
       },
     };
 
-    return <div className="quote-and-map-container">
-      <div className="quote-container">
+    return <div className="quote-container">
         <div className="quote-cover">
-          {quoteHelper[isEnglish][0]}<br/>
-          {quoteHelper[isEnglish][1]}
+          {quoteHelper[isEnglish][0]} <br/> {quoteHelper[isEnglish][1]}
         </div>
         <br/>
         <div className="quote-author">
-          <i>{quoteHelper[isEnglish][2]}</i><br/>
-          {quoteHelper[isEnglish][3]}
+          <i>{quoteHelper[isEnglish][2]}</i> {quoteHelper[isEnglish][3]}
         </div>
-      </div>
-      {africaMap()}
-      </div>
+    </div>
   };
 
   getGrid() {
@@ -91,6 +130,7 @@ export default class Home extends React.Component {
     return <div className="home-container">
       {this.getLanding()}
       {this.getQuote()}
+      {this.getNamibiaDesc()}
       {this.getGrid()}
     </div>;
   }
