5da847020ff19d278a201fed487fcbe3b157b851
Ece Ozalp <ozale272@newschool.edu>
2018-01-10 22:14:24 -0500

    adds styled banner to schedule, starts building highlights on schedule

--- src/main/react/components/schedule.jsx
+++ src/main/react/components/schedule.jsx
@@ -1,20 +1,41 @@
 import React from "react";
 import scheduleBanner from "../../resources/static/images/schedule/schedule_banner.jpg"
-import {scheduleHelper} from "../../resources/static/constants";
+import {scheduleBannerOverlay, highlightsHelper} from "../../resources/static/constants";
+import africaMap from '../../resources/static/vectors/africaMap';
 
 export default function Schedule({isEnglish}) {
 
   const getBanner = () => {
-    return <figure>
+    return <figure className="schedule-banner">
       <img src={scheduleBanner}/>
     </figure>
   };
 
+  const getBannerOverlay = () => {
+    return <div className="schedule-banner-overlay">
+      <div className="schedule-banner-overlay-contents">
+        <div className="schedule-overlay-title">{scheduleBannerOverlay[isEnglish][0]}</div>
+        <div className="schedule-overlay-caption">{scheduleBannerOverlay[isEnglish][1]}</div>
+        <div className="schedule-overlay-description">{scheduleBannerOverlay[isEnglish][2]}</div>
+      </div>
+    </div>
+  };
+
   const getHighlights = () => {
-    // textured background
-    // map
-    // highlights (locations / activities)
-  }
+    return <div className="schedule-highlights">
+      <div className="highlights-right-container">
+        <div className="schedule-trip-highlights"></div>
+        <div className="highlights-label"></div>
+        <div className="highlights-item"></div>
+        <div className="highlights-label"></div>
+      </div>
+      <div className="highlights-left-container">
+        <div className="map-container">
+          {africaMap()}
+        </div>
+      </div>
+    </div>
+  };
 
   const getDay = () => {
     // title
@@ -23,8 +44,12 @@ export default function Schedule({isEnglish}) {
     // meal
   };
 
-  return <div className="home-container">
-    "Hello World"
+  return <div>
+    {getBanner()}
+    <div className="schedule-container">
+      {getBannerOverlay()}
+      {getHighlights()}
+    </div>
   </div>;
 
 }//
